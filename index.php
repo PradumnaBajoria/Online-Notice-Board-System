9d6234289791d79703406b3d59e39d455d4f49e9
Pradumna <pradumnabajoria@gmail.com>
2021-02-11 16:56:03 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Online Notice Board System</title>
+
+    <!-- BOOTSTRAP FILE -->
+    <link rel="stylesheet" type = "text/>css" href="bootstrap-4.6.0-dist/css/bootstrap.min.css">
+    <script src="bootstrap-4.6.0-dist/js/bootstrap.min.js" charset="utf-8"></script>
+
+    <!-- CSS FILE -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <!--Header Section Starts-->
+    <div class="row" id = "header">
+      <div class="col-md-4">
+
+      </div>
+      <div class="col-md-4">
+        <h3>Online Notice Board System</h3>
+
+      </div>
+      <div class="col-md-4">
+
+      </div>
+    </div>
+
+    <!--Login Section Starts-->
+    
+
+  </body>
+</html>
